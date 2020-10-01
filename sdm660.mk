64659d6c56ac374e7d2a49f5fe27388903871454
Lunarixus <Nathanb644@hotmail.co.uk>
2020-10-01 16:41:11 +0000

    sdm660-common: Remove packages at build time

--- sdm660.mk
+++ sdm660.mk
@@ -344,6 +344,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     charger_res_images \
     product_charger_res_images
+    
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages    
 
 # Power
 PRODUCT_PACKAGES += \
