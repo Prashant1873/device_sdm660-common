7652872db001da6d44cdf25af623a37f4f336306
4j17h <7hou74m4j17h@gmail.com>
2020-12-14 01:26:32 +0530

    sdm660-common: XiaomiParts: Doze: Fix null proximity sensor
    Signed-off-by: 4j17h <7hou74m4j17h@gmail.com>
    Change-Id: Ib6fedcf02fd1ebe32700cfbbbedaa3842caaa634
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>

--- DeviceDoze/src/com/advanced/settings/doze/ProximitySensor.java
+++ DeviceDoze/src/com/advanced/settings/doze/ProximitySensor.java
@@ -51,7 +51,7 @@ public class ProximitySensor implements SensorEventListener {
     public ProximitySensor(Context context) {
         mContext = context;
         mSensorManager = mContext.getSystemService(SensorManager.class);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY, false);
+        mSensor = DozeUtils.getSensor(mSensorManager, "android.sensor.proximity");
         mExecutorService = Executors.newSingleThreadExecutor();
     }
 
